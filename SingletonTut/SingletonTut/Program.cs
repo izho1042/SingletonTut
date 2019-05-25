d82bd3bce7ca158b13e524ae196340a7320a9d21
WIN10-IZHO1042\Ian Zhou <ian.1994@live.cn>
2019-05-25 11:54:19 +1000

    Completed Singleton

---
+++ SingletonTut/SingletonTut/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonTut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton a = Singleton.GetInstance();
+            Singleton b = Singleton.GetInstance();
+
+            if (ReferenceEquals(a, b))
+            {
+                Console.WriteLine("These are the same instance."); // Print the result to the console.
+            }
+            else
+            {
+                Console.WriteLine("These are different instances."); // Print the result to the console.
+            }
+            Console.ReadKey(); // Hold the console, until a key on the keyboard is inserted.
+        }
+    }
+}
---
+++ SingletonTut/SingletonTut/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonTut
+{
+    public class Singleton
+    {
+        // Our Unique Singleton Instance
+        private static Singleton uniqueInstance;
+
+        // Private Constructor
+        private Singleton() { }
+
+        // Retrieve your unique instance
+        public static Singleton GetInstance()
+        {
+            if (uniqueInstance == null)
+            {
+                uniqueInstance = new Singleton();
+            }
+            return uniqueInstance;
+        }
+    }
+}
